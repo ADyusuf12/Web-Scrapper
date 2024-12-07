e5cbac1d9a0936b58acc45f394550ac7ee26cfe3
ADyusuf12 <adyusuf68@gmail.com>
2024-12-07 23:35:01 +0100

    error handling, cmd line args(specify pages), logging, userfriendly output

--- scrapper.py
+++ scrapper.py
@@ -1,36 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
+import argparse
+import logging
 
-res = requests.get('https://news.ycombinator.com/news')
-res2 = requests.get('https://news.ycombinator.com/news?p=2')
-soup = BeautifulSoup(res.text, 'html.parser')
-soup2 = BeautifulSoup(res2.text, 'html.parser')
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-links = soup.select('.titleline > a')
-subtext = soup.select('.subtext')
-links2 = soup2.select('.titleline > a')
-subtext2 = soup2.select('.subtext')
+def fetch_page_data(url):
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        return BeautifulSoup(res.text, 'html.parser')
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching data from {url}: {e}")
+        return None
 
-mega_links = links + links2
-mega_subtext = subtext + subtext2
+def get_hn_data(pages):
+    links, subtext = [], []
+    for page in range(1, pages + 1):
+        url = f'https://news.ycombinator.com/news?p={page}'
+        soup = fetch_page_data(url)
+        if soup:
+            links.extend(soup.select('.titleline > a'))
+            subtext.extend(soup.select('.subtext'))
+    return links, subtext
 
 def sort_by_votes(hnlist):
-	return sorted(hnlist, key=lambda k:k['votes'], reverse = True)
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 def custom_hn(links, subtext):
-	hn = []
-	for idx, item in enumerate(links):
-		title = links[idx].getText()
-		href = links[idx].get('href', None)	
-		vote = subtext[idx].select('.score')
-		if len(vote):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if vote:
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_by_votes(hn)
 
-			points = int(vote[0].getText().replace(' points', ''))
+def main():
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description='Scrape Hacker News for the top articles.')
+    parser.add_argument('pages', type=int, help='Number of pages to scrape')
+    args = parser.parse_args()
 
-			if points > 99:
-				hn.append({'title': title, 'link': href, 'votes': points})
+    logging.info(f"Scraping {args.pages} pages of Hacker News")
+    links, subtext = get_hn_data(args.pages)
+    top_articles = custom_hn(links, subtext)
+    
+    pprint.pprint(top_articles)
 
-	return sort_by_votes(hn)
-
-pprint.pprint(custom_hn(mega_links, mega_subtext))
+if __name__ == '__main__':
+    main()
